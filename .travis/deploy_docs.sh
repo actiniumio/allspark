57673d14844c568847616bc578f3ed7b3e26c20d
Germain <germain.gau@gmail.com>
2018-07-12 08:34:43 +0200

    Ensured firectory out/ exists whe running a branch build

--- .travis/deploy_docs.sh
+++ .travis/deploy_docs.sh
@@ -17,6 +17,7 @@ function doCompile {
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
+    mkdir out
     doCompile
     exit 0
 fi
